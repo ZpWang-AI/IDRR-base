cd7a9899fc8f87163d505d99dfb59b7e34a0c6ef
zpwang <zhipangwang@gmail.com>
2023-10-07 19:27:35 +0800

    modified:   corpusDatasets.py

--- corpusDatasets.py
+++ corpusDatasets.py
@@ -1,16 +1,51 @@
+import numpy as np
 import pandas as pd
 
+from typing import *
 from transformers import AutoTokenizer, DataCollatorWithPadding
 from torch.utils.data import Dataset
 
 
 class CustomDataset(Dataset):
-    def __init__(self, df:pd.DataFrame, tokenizer, label_map) -> None:
+    def __init__(
+        self,
+        df:pd.DataFrame, 
+        tokenizer, 
+        label_map,
+        label_level,
+        le_pos,
+        le_neg,
+    ) -> None:
         super().__init__()
         
         self.df = df
         self.tokenizer = tokenizer
         self.label_map = label_map
+        self.label_level = label_level
+        self.le_pos = le_pos
+        self.le_neg = le_neg
+    
+    def label_to_id(self, sense):
+        if self.label_level == 'level1':
+            label_id = self.label_map[sense.split('.')[0]]
+        else:
+            raise ValueError('wrong label_level')
+        return label_id
+    
+    def generate_label(self, sense_list:List[str]):
+        label = np.zeros(len(self.label_map))
+
+        sense1 = sense_list[0]
+        label[self.label_to_id(sense1)] += 1
+        
+        if self.le_pos:
+            for sense_ in sense_list[1:]:
+                if not pd.isna(sense_):
+                    label[self.label_to_id(sense_)] += 0.2
+        if self.le_neg:
+            label -= 0.1
+            
+        return label
         
     def __getitem__(self, index):
         row = self.df.iloc[index]
@@ -28,10 +63,9 @@ class CustomDataset(Dataset):
             max_length=256,
         )
         
-        label_id = self.label_map[conn1sense1.split('.')[0]]
-        label = [0]*len(self.label_map)
-        label[label_id] = 1
-        model_inputs['label'] = label
+        model_inputs['label'] = self.generate_label(
+            [conn1sense1, conn1sense2, conn2sense1, conn2sense2]
+        )
     
         return model_inputs
         
@@ -40,12 +74,23 @@ class CustomDataset(Dataset):
 
 
 class CustomCorpusDatasets():
-    def __init__(self, file_path, data_name='pdtb2', label_level='level1', model_name_or_path='roberta-base', logger=None):
+    def __init__(
+        self, 
+        file_path,
+        data_name='pdtb2',
+        model_name_or_path='roberta-base',
+        logger=None,
+        
+        label_level='level1',
+        label_expansion_positive=True,
+        label_expansion_negative=True,
+    ):
         assert data_name in ['pdtb2', 'pdtb3', 'conll']
         tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
         self.sep_t = tokenizer.sep_token
         self.cls_t = tokenizer.cls_token
         self.tokenizer = tokenizer 
+        self.data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
         
         if label_level == 'level1':
             label_list = 'Temporal Comparison Contingency Expansion'.split()
@@ -60,23 +105,28 @@ class CustomCorpusDatasets():
                                              'Conn2SemClass1', 'Conn2SemClass2'])
         df = df[df['Relation'] == 'Implicit']
 
+        self.label_level = label_level
+        self.le_pos = label_expansion_positive
+        self.le_neg = label_expansion_negative
+        dataset_kwargs = {
+            'tokenizer': tokenizer,
+            'label_map': self.label_map,
+            'label_level': label_level,
+            'le_pos': label_expansion_positive,
+            'le_neg': label_expansion_negative,
+        }
         self.train_dataset = CustomDataset(
             df=df[~df['Section'].isin([0, 1, 21, 22, 23, 24])], 
-            tokenizer=tokenizer, 
-            label_map=self.label_map,
+            **dataset_kwargs,
         )
         self.dev_dataset = CustomDataset(
             df=df[df['Section'].isin([0, 1])], 
-            tokenizer=tokenizer, 
-            label_map=self.label_map,
+            **dataset_kwargs,
         )
         self.test_dataset = CustomDataset(
             df=df[df['Section'].isin([21, 22])],
-            tokenizer=tokenizer, 
-            label_map=self.label_map,
+            **dataset_kwargs,
         )
-
-        self.data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
         
         if logger is not None:
             logger.info('-' * 30)
@@ -91,11 +141,15 @@ if __name__ == '__main__':
     from utils import get_logger
     os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
-    sample_dataset = CustomCorpusDatasets(r'D:\0--data\projects\04.01-IDRR数据\IDRR-base\CorpusData\PDTB2\pdtb2.csv',
-                                    data_name='pdtb2',
-                                    label_level='level1',
-                                    model_name_or_path='roberta-base',
-                                    logger=get_logger())
+    sample_dataset = CustomCorpusDatasets(
+        r'D:\0--data\projects\04.01-IDRR数据\IDRR-base\CorpusData\PDTB2\pdtb2.csv',
+        data_name='pdtb2',
+        label_level='level1',
+        model_name_or_path='roberta-base',
+        logger=get_logger(),
+        label_expansion_positive=True,
+        label_expansion_negative=True,
+    )
     for p in sample_dataset.train_dataset:
         print(p)
         break

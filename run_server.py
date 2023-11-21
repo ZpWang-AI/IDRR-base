8a2b16496391eaf10b272a70fe64fe70573b2ce2
zpwang <zhipangwang@gmail.com>
2023-11-21 16:42:27 +0800

    modified:   run_server.py

--- run_server.py
+++ run_server.py
@@ -54,9 +54,9 @@ def server_long_args(data_name='pdtb2', label_level='level1'):
     args.learning_rate = 3e-5
     args.warmup_ratio = 0.1
     
-    args.version = SERVER_NAME+'long_bs32*2_sec1'
+    args.version = SERVER_NAME+'long_best'
     args.train_batch_size = 32
-    args.secondary_label_weight = 1
+    args.secondary_label_weight = 0.5
     args.cuda_cnt = 2
     args.eval_per_epoch = 4
     args.gradient_accumulation_steps = 1

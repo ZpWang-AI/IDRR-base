5ae584d7b136bcc9d68d077d8ea4763a2194f636
zpwang <zhipangwang@gmail.com>
2023-11-28 21:19:34 +0800

    modified:   run_server.py

--- run_server.py
+++ run_server.py
@@ -50,29 +50,28 @@ def server_experiment_args(args=None):
     if args is None:
         args = server_base_args(test_setting=False)
     
-    args.secondary_label_weight = 0.
+    args.secondary_label_weight = 0.5
     args.data_augmentation_secondary_label = False
     args.data_augmentation_connective_arg2 = False
     
-    args.cuda_cnt = 1
+    args.cuda_cnt = 2
     args.warmup_ratio = 0.05
-    args.epochs = 5
-    args.train_batch_size = 8
+    args.epochs = 10
+    args.train_batch_size = 32
     args.eval_batch_size = 32
     args.eval_steps = 100
     args.log_steps = 10
     args.gradient_accumulation_steps = 1
-    args.eval_per_epoch = -1
+    args.eval_per_epoch = 4
     
     args.weight_decay = 0.01
-    args.learning_rate = 5e-6
+    args.learning_rate = 3e-5
     
     args.version = 'base'
     return args
     
     
 if __name__ == '__main__':
-    
     def experiment_once():
         todo_args = server_base_args(test_setting=True, data_name='pdtb2')
         # todo_args = server_base_args(test_setting=True, data_name='pdtb2', label_level='level2')
@@ -99,12 +98,12 @@ if __name__ == '__main__':
         from main import main
          
         for epoch in [5,10,20,30]:
-            for mu in [5,10,20,30]:
+            for mu in [5,15,30,45]:
                 for batch_size in [32]:
                     todo_args = server_experiment_args()
 
                     # TODO: prepare args
-                    todo_args.version = f'epoch{epoch}_lr{mu}mu_bs{batch_size}*2'
+                    todo_args.version = f'epoch{epoch}_lr{mu}mu_bs{batch_size}^2'
                     todo_args.epochs = epoch
                     todo_args.learning_rate = float(f'{mu}e-6') 
                     todo_args.train_batch_size = batch_size             
@@ -120,6 +119,6 @@ if __name__ == '__main__':
                     
                     main(todo_args)
 
-    experiment_once()
-    # experiment_multi_times()
+    # experiment_once()
+    experiment_multi_times()
     pass
